ac91c6754159f5e240566cc7d8482877dd27137f
Isaac Aktam <34764644+isaac-altair@users.noreply.github.com>
2018-05-09 12:09:28 -0500

    Book list database. Khan Academy.
    1) What are your favourite books? You can make a database table to store them in! In this first step, create a table to store your list of books. It should have columns for id, name, and rating.
    2) Now, add three of your favourite books into the table.

---
+++ Book challenge.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+INSERT INTO books VALUES (1, "Foundation by Isaac Asimov", 5);
+INSERT INTO books VALUES (2, "Children of Underground by Korolenko", 5);
+INSERT INTO books VALUES (3, "Biography of John Nash", 5);
+SELECT * FROM books;
+
